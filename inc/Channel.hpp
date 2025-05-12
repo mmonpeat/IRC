e2a537dbb78c434b5400c8a7d1d277ae4c1a26b4
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-05-12 19:49:58 +0200

    Channel class construction

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,21 +2,34 @@
 #define CHANNEL_HPP
 
 #include <string>
+#include <iostream>
 #include <vector>
+#include "Client.hpp"
 
 class Client;
 
 class Channel
 {
 	private:
-		std::string			name;
-		std::vector<std::string>	client_nicks;
-		std::vector<Client*> 		operators;
-		std::vector<Client*> 		clients;//if a client becomes an operator, it's removed from clients and added to operators.
+		std::string					_name;
+		std::string					_topic;
+		std::vector<std::string>	_client_nicks;
+		std::vector<Client*> 		_operators;
+		std::vector<Client*> 		_clients;//if a client becomes an operator, it's removed from clients and added to operators.
 	public:
-		Channel();
-		~Channel();
+		Channel(std::string name, Client *client); //just an idea, as only clients can create channels
+		~Channel(void);
 
+		//void	addClient(Client *client);
+		//void	removeClient()
+
+		//functions
+		// 1. bool isChannelEmpty(void) const; cada vez que alguien se desconecte o este 
+		//kickeado mirar cuanta gente queda, en caso que zero eliminar el channel
+
+		//2. void changeTopic(std::string topic); tiene que cambiar el topic
+
+		//3. 
 };
 
 
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,5 +1,14 @@
 #include "Channel.hpp"
 
-Channel::Channel() {}
+Channel::Channel(std::string name, Client *client) : _name(name), _topic("No topic is set"){
+    //add the operator
+    client->getNick(); //esta aqui de chorrada para que no se queje el make
+    std::cout << "Channel " << this->_name << " constructor has been called" << std::endl;
+    return;
+}
 
-Channel::~Channel() {}
+Channel::~Channel(void) {
+    // check if there are allocated memory and delete it before destruction!!!
+    std::cout << "Channel " << this->_name << " destructor has been called" << std::endl;
+    return;
+}
