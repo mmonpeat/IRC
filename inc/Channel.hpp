054162b086a3ce17ec2c2f4c21cd7caa821efaa7
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-05-19 18:31:03 +0200

    Channel functions

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <sys/socket.h>
 #include "Client.hpp"
 
 class Client;
@@ -11,27 +12,36 @@ class Client;
 class Channel
 {
 	private:
-		std::string					_name;
-		std::string					_topic;
-		std::vector<std::string>	_client_nicks;
-		std::vector<Client*> 		_operators;
-		std::vector<Client*> 		_clients;
+		std::string				_name;
+		std::string				_topic;
+		std::vector<Client*>	_operators;
+		std::vector<Client*>	_clients;
+		//modes
+		bool					_limit_set;
+		bool					_invite_set;
+		bool					_topic_set;
+		bool					_password_set;
+		//vector of invited clients
+		int						_channel_limit;
+		std::string				_password;
+		
 	public:
-		Channel(std::string name, Client *client); //just an idea, as only clients can create channels
+		Channel(std::string name, Client *client);
 		~Channel(void);
 
-		//void	addClient(Client *client);
-		//void	removeClient()
+		void	addClient(Client *client);
+		void	removeClient(Client* client);
+		void	addOperator(Client *new_op);
+		void	removeOperator(Client *op);
+		bool	isClient(Client* client);
+		bool	isChannelEmpty(void) const; //llamar cada vez que alguien haga quit
+		
 
-		//functions
-		// 1. bool isChannelEmpty(void) const; llamar cada vez que alguien haga quit
-
-	
 		// Channel functions for ops
-		bool	hasPerms();
-		void	changeTopic(std::string topic, std::string nick);
-		//void	kickUser();
-		//void	changeMode();
+		bool	isOperator(std::string nick) const;
+		void	changeTopic(std::string topic, Client* client);
+		void	kickUser(Client* kicker, Client* target);
+		//void	changeMode(Client* client, std::string command);
 };
 
 
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,8 +1,35 @@
+/* MODES EXPLAINED - DELETE LATER
+	[] - i – Invite-only channel, only users who have been invited can join. 
+		 We need to store a list of invited users (e.g., nicknames or fds)
+		 Default: off 
+	[] - t – Topic settable only by channel operators Prevents non-operators from changing the topic.
+		 Default: on (+t)
+	[] - k – Channel key (password) Users must provide a key (password) to join.  We need to store a std::string _key.
+		 +k <key> sets the key, -k removes it.
+		 Default: off
+	[] - o – Give/take operator privilege. Used to promote (+o) or demote (-o) a user to/from channel operator.
+		 You’ll be managing _operators (probably std::vector<Client*>)
+		 Note: This mode applies to a user, not globally.
+	[] - l – Set/remove user limit. Sets how many users can be in the channel.
+		You need an int _userLimit, active only when +l is set. +l <number> sets limit, -l removes it.
+		Default: off (no limit)
+*/
+
+/* Numeric replies for channel
+	403 - trying to change topic and not being an operator when mode is resctricted
+
+*/
+
 #include "Channel.hpp"
 
-Channel::Channel(std::string name, Client *client) : _name(name), _topic("No topic is set"){
-    //add the operator
+//set modes to default
+Channel::Channel(std::string name, Client* client) : _name(name), _topic("No topic is set"){
+    //addOperator(Client* client)
     client->getNick(); //esta aqui de chorrada para que no se queje el make
+	_limit_set = false;
+	_invite_set = false;
+	_topic_set = true;
+	_password_set = false;
     std::cout << "Channel " << this->_name << " constructor has been called" << std::endl;
     return;
 }
@@ -14,12 +41,57 @@ Channel::~Channel(void) {
 }
 
 
-//---------------------------------- Functions -------------------------------------------
+//---------------------------------- Class Functions -----------------------------------------
 
-void	Channel::changeTopic(std::string new_topic, std::string nick) {
-    if (this->_operators)
-	    this->_topic = new_topic;
-	// hay que escribir en el fds correctos habra que cambiar!!!!
-	std::cout << "Operarator's nick goes here" << " has changed the topic to: " << new_topic << std::endl;
+void	Channel::removeClient(Client* client) {
+
+}
+
+void	Channel::addClient(Client *client) {
+
+}
+
+bool	Channel::isClient(Client *client) {
+	return true;
+}
+
+bool	Channel::isChannelEmpty(void) const {
+
+	return true;
+}
+//---------------------------------- OPs Functions -------------------------------------------
+
+bool    Channel::isOperator(std::string nick) const {
+	for (std::vector<Client*>::const_iterator it = _operators.begin(); it != _operators.end(); ++it)
+	{
+		if (*it && (*it)->getNick() == nick)
+			return true;
+	}
+	return false;
+}
+
+
+void Channel::changeTopic(const std::string new_topic, Client* client) {
+	if (isOperator(client->getNick())) {
+		_topic = new_topic;
+
+		std::string message = ":" + client->getNick() + " TOPIC #" + _name + " :" + _topic + "\r\n";
+
+		for (std::vector<Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+		{
+			if (*it)
+				send((*it)->getFd(), message.c_str(), message.size(), 0);
+		}
+	}
+}
+
+//operators can kick other operators
+void	Channel::kickUser(Client* kicker, Client* target) {
+	if (isOperator(kicker->getNick()) == false) {
+		std::cout << "You have no rights to kick users in this chat" << std::endl;
+		return;
+	}
+	removeClient(target);
+	//maybe message to users of the channel
 	return;
 }
