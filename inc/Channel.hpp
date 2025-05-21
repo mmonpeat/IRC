edb6cea23b2f2c204069345711f58dd548179172
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-05-21 19:37:08 +0200

    all issues errors and MODE revises, we dont need some like RPL_BANLIST and RPL_ENDOFBANLIST

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -9,7 +9,7 @@ class Client;
 class Channel
 {
 	private:
-		std::string			name;
+		std::string					name;
 		std::vector<std::string>	client_nicks;
 		std::vector<Client*> 		operators;
 		std::vector<Client*> 		clients;//if a client becomes an operator, it's removed from clients and added to operators.
