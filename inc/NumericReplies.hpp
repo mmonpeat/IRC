e2147b4734e8b7167e243bd6602186435435495a
outsiderice <arantxamagnelli@gmail.com>
2025-06-12 18:22:35 +0200

    written some replies

--- inc/NumericReplies.hpp
+++ inc/NumericReplies.hpp
@@ -1,14 +1,42 @@
 #ifndef NUMERICREPLIES_HPP
 # define NUMERICREPLIES_HPP
 
-//command replies
+
+//might not use the numbers but for now i'm leaving them here
+//command codes
 # define	RPL_WELCOME				001
 
-//error replies
+//error codes
 # define	ERR_NONICKNAMEGIVEN		431
 # define	ERR_NICKNAMEINUSE		433
 # define	ERR_NOTREGISTERED		451
 # define	ERR_NEEDMOREPARAMS		461
 # define	ERR_ALREADYREGISTERED	462
+
+
+//Messages
+inline	std::string	rplWelcome(std::string nickname){
+	return (":localhost 001 " + nickname + " :Welcome to our IRC server, " + nickname + ":)\r\n");
+}
+
+inline	std::string	errNoNickNameGiven(void){
+	return (":localhost 431 :No nickname given\r\n"); //might need to be changed
+} 
+
+inline	std::string	errNickNameInUse(std::string nickname, std::string nick){
+	return (":localhost 433 " + nickname + " " + nick + " :Nickname is already in use\r\n");
+}
+
+inline	std::string	errNotRegistered(void){
+	return (":localhost 451 :You have not registered\r\n");
+}
+
+inline	std::string	errNeedMoreParams(std::string nickname, std::string command){
+	return (":localhost 461 " + nickname + command + " :Not enough parameters\r\n");
+}
+
+inline	std::string errAlreadyRegistered(std::string nickname){
+	return (":localhost 462 " + nickname + ":You may not reregister");
+}
 
 #endif
