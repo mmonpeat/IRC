9728957684651ad2f1f72d678949e461cc9bed33
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-07-11 15:30:27 +0200

    errors with operators

--- .build/src/main.d
+++
@@ -1,6 +0,0 @@
-.build/src/main.o: src/main.cpp inc/Server.hpp inc/Client.hpp \
- inc/Channel.hpp inc/NumericReplies.hpp
-inc/Server.hpp:
-inc/Client.hpp:
-inc/Channel.hpp:
-inc/NumericReplies.hpp:
--- .build/src/utilsJoin.d
+++
@@ -1,6 +0,0 @@
-.build/src/utilsJoin.o: src/utilsJoin.cpp inc/Server.hpp inc/Client.hpp \
- inc/Channel.hpp inc/NumericReplies.hpp
-inc/Server.hpp:
-inc/Client.hpp:
-inc/Channel.hpp:
-inc/NumericReplies.hpp:
