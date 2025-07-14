a06958ab715bae42075ed5651c87844e02b56143
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2025-07-14 15:50:24 +0200

    cleaning

--- .build/src/join.d
+++
@@ -1,6 +0,0 @@
-.build/src/join.o: src/join.cpp inc/Server.hpp inc/Client.hpp \
-  inc/Channel.hpp inc/NumericReplies.hpp
-inc/Server.hpp:
-inc/Client.hpp:
-inc/Channel.hpp:
-inc/NumericReplies.hpp:
--- .build/src/modes.d
+++
@@ -1,6 +0,0 @@
-.build/src/modes.o: src/modes.cpp inc/Server.hpp inc/Client.hpp \
-  inc/Channel.hpp inc/NumericReplies.hpp
-inc/Server.hpp:
-inc/Client.hpp:
-inc/Channel.hpp:
-inc/NumericReplies.hpp:
